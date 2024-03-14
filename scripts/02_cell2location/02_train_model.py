7601a74a701f21e178f5936c0cacaeb008338fa8
evaapo <e.apostolov@garvan.org.au>
2024-03-14 17:59:06 +1100

    scripts to run cell2location using PCa scRNA-seq reference

---
+++ scripts/02_cell2location/02_train_model.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python3
+
+# --- Aim
+# Train cell2location model to be used on many samples
+
+
+# --- Define environment
+# env: cell2location
+
+# --- Load packages
+import argparse
+import scanpy as sc
+import matplotlib.pyplot as plt
+import os
+
+from datetime import date
+
+# this line should go before importing cell2location
+os.environ["THEANO_FLAGS"] = 'device=cuda,floatX=float32,force_device=True'
+import cell2location
+
+from matplotlib import rcParams
+rcParams['pdf.fonttype'] = 42 # enables correct plotting of text for PDFs
+
+# Some imports for qc plots
+import numpy as np
+import matplotlib
+from scvi import REGISTRY_KEYS
+from scipy.sparse import issparse
+
+
+# --- Argparse arguments
+
+parser = argparse.ArgumentParser(description="Import variables & train Cell2Location model",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--projectName", help="Main project name")
+parser.add_argument("--repo", help="Repo name")
+parser.add_argument("--exp", help="Exp name")
+parser.add_argument("--analysis", help="Analysis step name")
+parser.add_argument("--sample_id", help="Sample ID")
+
+args = vars(parser.parse_args())
+
+# Create directory structure using argparse arguments
+projectName = args["projectName"]
+repo = args["repo"]
+exp = args["exp"]
+analysis = args["analysis"]
+sample_id = args["sample_id"]
+
+# Define directory paths
+repoDir = os.path.join('/share/ScratchGeneral/evaapo/projects/', projectName, repo)
+resultDir = os.path.join(repoDir, "results", exp, analysis)
+rObjectDir = os.path.join(resultDir, "rObjects")
+figDir = os.path.join(resultDir, "figures")
+tabDir = os.path.join(resultDir, "tables")
+qcDir = os.path.join(resultDir, 'model')
+
+
+# Ensure output directories exist
+os.makedirs(repoDir, exist_ok=True)
+os.makedirs(resultDir, exist_ok=True)
+os.makedirs(rObjectDir, exist_ok=True)
+os.makedirs(figDir, exist_ok=True)
+os.makedirs(tabDir, exist_ok=True)
+os.makedirs(qcDir, exist_ok=True)
+
+
+# today = date.today()
+# today = today.strftime("%Y%m%d")
+# out_dir = os.path.join(qc_dir, today)
+# os.makedirs(out_dir, exist_ok=True)
+
+# --- Load the single-cell ref
+
+# Use the miniatlas downloaded from cellxgene
+pca_atlas_dir = os.path.join(repoDir, 'results', exp, '01_h5ad_conversion', 'rObjects')
+pca_atlas_file = 'PCa_merged_filtered.h5ad'
+
+adata_ref = sc.read(os.path.join(pca_atlas_dir, pca_atlas_file))
+
+# Extract the raw data (cell2location wants raw data)
+adata_ref = adata_ref.raw.to_adata()
+
+# --- Filter genes
+
+from cell2location.utils.filtering import filter_genes
+selected = filter_genes(adata_ref, cell_count_cutoff=5, cell_percentage_cutoff2=0.03, nonz_mean_cutoff=1.12)
+
+# filter the object
+adata_ref = adata_ref[:, selected].copy()
+
+plt.savefig(os.path.join(figDir, 'genes_filtered.pdf'))
+
+plt.close()
+
+# --- Prepare for regression model
+
+# Arguments
+labels_key = 'celltype_minor_v2'
+donor_id = 'sample_id'
+batch_key = 'orig.ident' # this is the capture reaction (PCa1:PCa16)
+
+# prepare anndata for the regression model
+cell2location.models.RegressionModel.setup_anndata(adata=adata_ref,
+                        # 10X reaction / sample / batch
+                        batch_key=batch_key,
+                        # cell type, covariate used for constructing signatures
+                        labels_key=labels_key,
+                        # multiplicative technical effects (donor effect)
+                        categorical_covariate_keys=[donor_id]
+                       )
+
+
+# create the regression model
+# Note: ref needs to be raw (i.e. untransformed, unnormalized) for this to work
+# If it's not raw this step will throw an error
+from cell2location.models import RegressionModel
+mod = RegressionModel(adata_ref)
+
+# view anndata_setup as a sanity check
+mod.view_anndata_setup()
+
+
+# --- Training the model
+mod.train(max_epochs=250, use_gpu=True)
+
+# Determine if model needs more training
+mod.plot_history(20)
+
+plt.savefig(os.path.join(figDir, 'model_training.pdf'))
+plt.close()
+
+# In this section, we export the estimated cell abundance (summary of the posterior distribution).
+adata_ref = mod.export_posterior(
+    adata_ref, sample_kwargs={'num_samples': 1000, 'batch_size': 2500, 'use_gpu': True}
+)
+
+# Save model
+mod.save(os.path.join(figDir, f"reference_signatures"), overwrite=True)
+
+# Save anndata object with results
+adata_file = f"reference_signatures/sc.h5ad"
+adata_ref.write(os.path.join(figDir, adata_file))
+
+
+# --- Examine QC plots
+
+# Note: this plot doesn't seem to be plotted correctly, but not sure if I can do anything about it
+# Should be two separate plots but they're plotted on top of each other
+
+# mod.plot_QC()
+# plt.savefig(os.path.join(figDir, 'model_qc.pdf'))
+# plt.close()
+
+# Actually, take the source code to stop them being plotted on top of each other
+
+# Some default params
+summary_name = 'means'
+use_n_obs = 1000
+scale_average_detection = True
+
+# Plot 1: Reconstruction accuracy
+# Should be roughly diagonal
+
+if getattr(mod, "samples", False) is False:
+    raise RuntimeError("self.samples is missing, please run self.export_posterior() first")
+
+if use_n_obs is not None:
+    ind_x = np.random.choice(
+        mod.adata_manager.adata.n_obs, np.min((use_n_obs, mod.adata.n_obs)), replace=False
+    )
+else:
+    ind_x = None
+
+mod.expected_nb_param = mod.module.model.compute_expected(
+    mod.samples[f"post_sample_{summary_name}"], mod.adata_manager, ind_x=ind_x
+)
+x_data = mod.adata_manager.get_from_registry(REGISTRY_KEYS.X_KEY)[ind_x, :]
+
+if issparse(x_data):
+    x_data = np.asarray(x_data.toarray())
+
+mod.plot_posterior_mu_vs_data(mod.expected_nb_param["mu"], x_data)
+
+plt.savefig(os.path.join(qcDir, 'model_qc-1.pdf'))
+
+plt.close()
+
+
+# Plot 2: Estimated reference expression signatures comp. to average expression in cluster
+# Should look like a perfect diagonal
+
+inf_aver = mod.samples[f"post_sample_{summary_name}"]["per_cluster_mu_fg"].T
+if scale_average_detection and ("detection_y_c" in list(mod.samples[f"post_sample_{summary_name}"].keys())):
+    inf_aver = inf_aver * mod.samples[f"post_sample_{summary_name}"]["detection_y_c"].mean()
+
+aver = mod._compute_cluster_averages(key=REGISTRY_KEYS.LABELS_KEY)
+aver = aver[mod.factor_names_]
+
+plt.hist2d(
+    np.log10(aver.values.flatten() + 1),
+    np.log10(inf_aver.flatten() + 1),
+    bins=50,
+    norm=matplotlib.colors.LogNorm(),
+)
+plt.xlabel("Mean expression for every gene in every cluster")
+plt.ylabel("Estimated expression for every gene in every cluster")
+plt.show()
+
+plt.savefig(os.path.join(qcDir, 'model_qc-2.pdf'))
+
+plt.close()
+
+# --- Export cell type signatures
+
+# export estimated expression in each cluster
+if 'means_per_cluster_mu_fg' in adata_ref.varm.keys():
+    inf_aver = adata_ref.varm['means_per_cluster_mu_fg'][[f'means_per_cluster_mu_fg_{i}'
+                                    for i in adata_ref.uns['mod']['factor_names']]].copy()
+else:
+    inf_aver = adata_ref.var[[f'means_per_cluster_mu_fg_{i}'
+                                    for i in adata_ref.uns['mod']['factor_names']]].copy()
+
+inf_aver.columns = adata_ref.uns['mod']['factor_names']
+inf_aver.iloc[0:5, 0:5]
+
+# Save this as a table
+inf_aver.to_csv(os.path.join(tabDir, 'reference_cell_type_signatures.csv'))
+
+# --- Finish
+
+print("Script succesfully completed")
---
+++ scripts/02_cell2location/04_visualize_results.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+# --- Aim
+# Use cell2location for deconvolution
+# Plot cell type mapping results for quick check (does it look as expected)
+
+
+# --- Define environment
+# env: cell2location
+
+# --- Load packages
+import argparse
+import scanpy as sc
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from datetime import datetime
+
+from matplotlib import rcParams
+rcParams['pdf.fonttype'] = 42 # enables correct plotting of text for PDFs
+
+import pandas as pd
+import os
+
+# --- Argparse arguments                
+parser = argparse.ArgumentParser(description="Import variables & train Cell2Location model",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--projectName", help="Main project name")
+parser.add_argument("--repo", help="Repo name")
+parser.add_argument("--exp", help="Exp name")
+parser.add_argument("--analysis", help="Analysis step name")
+parser.add_argument("--sample_id", help="Sample ID")
+parser.add_argument("--h5ad_dir", help="Path to directory for .h5ad output files - Visium raw data")
+parser.add_argument("--model_dir", help="Path to directory for output files from model training")
+
+args = vars(parser.parse_args())
+
+# Create directory structure using argparse arguments
+projectName = args["projectName"]
+repo = args["repo"]
+exp = args["exp"]
+analysis = args["analysis"]
+sample_id = args["sample_id"]
+h5ad_dir = args["h5ad_dir"]   # /share/ScratchGeneral/evaapo/projects/PCa_Visium/pca_visium_analysis/data/anndata_objects/
+model_dir = args["model_dir"]   # /share/ScratchGeneral/evaapo/projects/PCa_Visium/pca_visium_analysis/data/anndata_objects/
+
+# Define directory paths
+repoDir = os.path.join('/share/ScratchGeneral/evaapo/projects/', projectName, repo)
+resultDir = os.path.join(repoDir, "results", exp, analysis, sample_id)
+objectDir = os.path.join(resultDir, "objects")
+figDir = os.path.join(resultDir, "figures")
+tabDir = os.path.join(resultDir, "tables")
+                        
+# Ensure output directories exist
+os.makedirs(repoDir, exist_ok=True)
+os.makedirs(resultDir, exist_ok=True)
+os.makedirs(objectDir, exist_ok=True)
+os.makedirs(figDir, exist_ok=True)
+os.makedirs(tabDir, exist_ok=True)
+
+
+# inDir for results from previous step
+inDir = os.path.join("/share/ScratchGeneral/evaapo/projects/PCa_Visium/pca_visium_analysis/results/02_cell2location/03a_cell_type_mapping", sample_id, "objects")
+
+# --- Extract some info from the sample sheet
+
+# Extract the sample_id
+sample_dir = os.path.join(repoDir, 'config')
+sample_sheet_file = 'sample_sheet.csv'
+
+sample_sheet = pd.read_csv(os.path.join(sample_dir, sample_sheet_file))
+
+# Filter the DataFrame based on the condition sample_id == sample_id
+filtered_df = sample_sheet[sample_sheet['sample_id'] == sample_id]
+
+# Extract the sample_name from the first row (assuming there's only one match)
+file_id = filtered_df['sample_id'].iloc[0]
+
+
+# --- Load the cell2location output
+
+# Load results
+adata_file = f"{file_id}_cell2location.h5ad"
+adata_vis = sc.read_h5ad(os.path.join(inDir, adata_file))
+
+
+# --- Visualizing cell abundance in spatial coordinates
+
+# add 5% quantile, representing confident cell abundance, 'at least this amount is present',
+# to adata.obs with nice names for plotting
+adata_vis.obs[adata_vis.uns['mod']['factor_names']] = adata_vis.obsm['q05_cell_abundance_w_sf']
+
+# Extract cell type names
+cell_types = adata_vis.uns['mod']['factor_names']
+
+with mpl.rc_context({'axes.facecolor':  'black',
+                     'figure.figsize': [4.5, 5]}):
+    sc.pl.spatial(adata_vis, cmap='magma',
+                  # show first 8 cell types
+                  color=cell_types,
+                  ncols=4, size=1.3,
+                  img_key='hires',
+                  # limit color scale at 99.2% quantile of cell abundance
+                  vmin=0, vmax='p99.2'
+                 )
+
+plt.savefig(os.path.join(figDir, 'cell_type_abundance_spatial.pdf'))
+plt.close('all')
+
+# Now we use cell2location plotter that allows showing multiple cell types in one panel
+from cell2location.plt import plot_spatial
+
+# select up to 6 clusters
+clust_labels = ['uniCAFs', 'NPF_like', 'whCAFs', 'pnCAFs', 'pSMCs', 'vSMCs', 'Pericytes']
+clust_col = ['' + str(i) for i in clust_labels] # in case column names differ from labels
+
+with mpl.rc_context({'figure.figsize': (15, 15)}):
+    fig = plot_spatial(
+        adata=adata_vis,
+        # labels to show on a plot
+        color=clust_col, labels=clust_labels,
+        show_img=True,
+        # 'fast' (white background) or 'dark_background'
+        style='fast',
+        # limit color scale at 99.2% quantile of cell abundance
+        max_color_quantile=0.992,
+        # size of locations (adjust depending on figure size)
+        circle_diameter=6,
+        colorbar_position='right'
+    )
+
+plt.savefig(os.path.join(out_dir_plots, 'selected_cell_types.pdf'))
+plt.close('all')
+
+
+print('Script succesfully completed')
