bb55ef4e33ee7f0d3821d80c5f9d9d3d2fbc8d5e
evaapo <e.apostolov@garvan.org.au>
2024-03-14 17:58:07 +1100

    param files for different scripts

---
+++ config/02_train_model_params.sh
@@ -0,0 +1,39 @@
+# Parameters for submission scripts (submit_universal.sh & submit_dict.sh)
+
+# --- Directories
+
+# 01 PROJECT
+projectName="PCa_Visium"
+# 02 REPO NAME
+repo="pca_visium_analysis"
+# 03 EXP CODE
+exp="02_cell2location"
+# 04 ANALYSIS
+analysis="02_train_model"
+# 05 SAMPLE ID
+sample_id="PCa_atlas"
+#  PATH TO SINGLE CELL REFERENCE
+
+## --- Other Args (for QC) 
+## 06 MIN COUNT
+#min_count=1000
+## 07 MIN GENE
+#min_gene=200
+## min_spots=3
+
+# 08 ENVIRONMENT
+environment="cell2location"
+
+# 09 DATA TO USE (Which data to use - different normalization methods: SCTransform or scanpy's log norm)
+use_data="log_norm"
+
+# make a log directory
+logDir="/share/ScratchGeneral/evaapo/projects/${projectName}/${repo}/results/${exp}/${analysis}/logs/"
+mkdir -p $logDir
+
+# seed
+seed=42
+
+### Visium sample sheet: contains the names of the visium samples
+##config_dir="$project_dir/config"
+##visium_sample_sheet="$config_dir/sample_sheet.csv"
---
+++ config/03a_celltype_mapping_params.sh
@@ -0,0 +1,24 @@
+# Parameters for submission scripts (submit_universal.sh & submit_dict.sh)
+
+# --- Directories
+
+# 01 PROJECT
+projectName="PCa_Visium"
+# 02 REPO NAME
+repo="pca_visium_analysis"
+# 03 EXP CODE
+exp="02_cell2location"
+# 04 ANALYSIS
+analysis="03a_cell_type_mapping"
+# 05 SAMPLE ID
+# sample_id="PCa_atlas"
+
+# 08 ENVIRONMENT
+environment="cell2location"
+
+# Paths to downstream scripts
+h5ad_dir="/share/ScratchGeneral/evaapo/projects/PCa_Visium/pca_visium_analysis/data/anndata_objects/"
+model_dir="/share/ScratchGeneral/evaapo/projects/PCa_Visium/pca_visium_analysis/results/02_cell2location/02_train_model/figures/reference_signatures/"
+
+# Environment to use
+environment="cell2location"
