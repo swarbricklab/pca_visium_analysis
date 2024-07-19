f75f333e1adb78fa668ce255020ca8c5a76a1dd1
evaapo <e.apostolov@garvan.org.au>
2024-07-19 18:19:09 +1000

    added PCa Visium Batch 3 results to dvc

---
+++ random.py
@@ -0,0 +1,17 @@
+# --- Arguments
+project_dir = '/share/ScratchGeneral/evaapo/projects/PCa_Visium/pca_visium_analysis'
+sample_id = '20033'
+use_data = 'log_norm'
+
+# data_dir = args["data_dir"]
+res_dir = os.path.join(project_dir, 'results')
+h5ad_dir = os.path.join(project_dir, 'data', 'anndata_objects')
+log_dir = os.path.join(project_dir, 'logs')
+config_dir = os.path.join(project_dir, 'config')
+visium_sample_sheet = os.path.join(config_dir, 'sample_sheet.csv')
+
+
+# Other arguments (for qc)
+min_count=1000
+min_gene=200
+min_spots=3
